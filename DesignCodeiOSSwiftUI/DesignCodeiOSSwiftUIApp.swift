8f0bdfee16158aec3d79626fbef17871bc9d5d79
Vladislav Yakubets <vladislav.yakubets@codabrasoft.us>
2022-02-15 14:41:22 +0300

    add initial project files

---
+++ DesignCodeiOSSwiftUI/DesignCodeiOSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOSSwiftUIApp.swift
+//  DesignCodeiOSSwiftUI
+//
+//  Created by Vladislav Yakubets on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
