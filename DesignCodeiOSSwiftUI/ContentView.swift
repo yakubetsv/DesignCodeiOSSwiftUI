43dd8c790bfeb9ce441c3c635296e66ccb662497
VY <7965474@gmail.com>
2022-02-16 16:15:56 +0300

    main screen content view

--- DesignCodeiOSSwiftUI/ContentView.swift
+++ DesignCodeiOSSwiftUI/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0  )
+            Text("Swift UI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all, 20)
+        .frame(height: 350)
+        .background(.background)
+        .cornerRadius(30)
+        .shadow(radius: 20)
     }
 }
 
