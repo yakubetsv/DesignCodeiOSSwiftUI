82ed0120af0422953f79cd1c1ff98de6c3fc4d49
VY <7965474@gmail.com>
2022-02-17 12:19:23 +0300

    lesson 3

--- DesignCodeiOSSwiftUI/ContentView.swift
+++ DesignCodeiOSSwiftUI/ContentView.swift
@@ -19,19 +19,24 @@ struct ContentView: View {
             Text("Swift UI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
+                .foregroundColor(.secondary)
         }
         .padding(.all, 20)
+        .padding(.vertical, 20)
         .frame(height: 350)
         .background(.background)
         .cornerRadius(30)
-        .shadow(radius: 20)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10.0, x: 0, y: 10)
+        .padding(.horizontal, 20)
     }
 }
 
