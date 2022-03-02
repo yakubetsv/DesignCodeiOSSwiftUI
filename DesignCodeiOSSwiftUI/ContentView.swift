6915892e5efebaedea8731725dee27e4624b16d4
VY <7965474@gmail.com>
2022-03-02 13:43:11 +0300

    lecture 4: material and foreground styles

--- DesignCodeiOSSwiftUI/ContentView.swift
+++ DesignCodeiOSSwiftUI/ContentView.swift
@@ -19,10 +19,17 @@ struct ContentView: View {
             Text("Swift UI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(
+                    .linearGradient(
+                        colors: [.primary, .primary.opacity(0.5)],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
             Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
@@ -33,15 +40,25 @@ struct ContentView: View {
         .padding(.all, 20)
         .padding(.vertical, 20)
         .frame(height: 350)
-        .background(.background)
+        .background(.ultraThinMaterial)
         .cornerRadius(30)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10.0, x: 0, y: 10)
         .padding(.horizontal, 20)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
